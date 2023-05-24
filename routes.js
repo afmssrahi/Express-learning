a5f2405820989fd82410ff08ed3560256db99dc2
A. F. M Shahriar Sharif (Rahi) <afmssrahi@gmail.com>
2023-05-24 20:30:16 +0600

    :art: refactor: separate controllers

--- routes.js
+++ routes.js
@@ -1,41 +1,16 @@
 const router = require('express').Router();
-const fs = require('fs');
 
-router.get('/', (req, res) => {
-	fs.readFile('./pages/index.html', (err, data) => {
-		if (err) {
-			console.log('Error: ', err);
-			res.send('<h1>Something went wrong</h1>');
-		} else {
-			res.write(data);
-			res.end();
-		}
-	});
-});
+const {
+	homeController,
+	aboutController,
+	helpController,
+} = require('./controller');
 
-router.get('/about', localMiddleware, (req, res) => {
-	fs.readFile('./pages/about.html', (err, data) => {
-		if (err) {
-			console.log('Error: ', err);
-			res.send('<h1>Something went wrong</h1>');
-		} else {
-			res.write(data);
-			res.end();
-		}
-	});
-});
+router.get('/', homeController);
 
-router.get('/help', (req, res) => {
-	fs.readFile('./pages/help.html', (err, data) => {
-		if (err) {
-			console.log('Error: ', err);
-			res.send('<h1>something went wrong</h1>');
-		} else {
-			res.write(data);
-			res.end();
-		}
-	});
-});
+router.get('/about', localMiddleware, aboutController);
+
+router.get('/help', helpController);
 
 function localMiddleware(req, res, next) {
 	console.log('I am local middleware');
