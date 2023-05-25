d7b718a661e11e7a1d03059417a95af7185ce3ef
A. F. M Shahriar Sharif (Rahi) <afmssrahi@gmail.com>
2023-05-26 03:24:30 +0600

    :feat: create pages

--- controller.js
+++ controller.js
@@ -1,30 +1,12 @@
-const fs = require('fs');
-
 exports.homeController = (req, res) => {
 	const title = 'Dynamic Title';
-	res.render('home', { title });
+	res.render('pages/home', { title });
 };
 
 exports.aboutController = (req, res) => {
-	fs.readFile('./pages/about.html', (err, data) => {
-		if (err) {
-			console.log('Error: ', err);
-			res.send('<h1>Something went wrong</h1>');
-		} else {
-			res.write(data);
-			res.end();
-		}
-	});
+	res.render('pages/about');
 };
 
 exports.helpController = (req, res) => {
-	fs.readFile('./pages/help.html', (err, data) => {
-		if (err) {
-			console.log('Error: ', err);
-			res.send('<h1>something went wrong</h1>');
-		} else {
-			res.write(data);
-			res.end();
-		}
-	});
+	res.render('pages/help');
 };
